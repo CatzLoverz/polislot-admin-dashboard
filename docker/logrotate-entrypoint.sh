a97ed27ffc2d1c6a9e33bde1389247c0804058f6
CatzLoverz <atmaja123putra@gmail.com>
2026-02-22 22:56:13 +0700

    update entrypoint for logrotate
    - fix file permission so host user can see database logs

--- docker/logrotate-entrypoint.sh
+++ docker/logrotate-entrypoint.sh
@@ -63,6 +63,11 @@ EOF
 chmod 644 /etc/logrotate.d/mariadb
 chown root:root /etc/logrotate.d/mariadb
 
-# 3. RUN CRON
+# 5. ADD MINUTELY CRON JOB TO FIX PERMISSIONS
+# Tambahkan cron job 1 menit untuk memastikan file log baru yang dibuat MariaDB 
+# selalu bisa dibaca dari host OS (chmod 644).
+echo "* * * * * find /var/log/mysql -type f -name '*.log' -exec chmod 644 {} +" >> /etc/crontabs/root
+
+# 6. RUN CRON
 echo "Starting crond..."
 exec /usr/sbin/crond -f -L 8
